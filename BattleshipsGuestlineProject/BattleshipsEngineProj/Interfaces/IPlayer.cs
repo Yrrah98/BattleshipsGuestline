6a67f61ccd13cc690d0ba546ea4168f80c513c40
Yrrah98 <harryjones101998@hotmail.com>
2021-02-17 21:54:21 +0000

    Added the player and the stub of some small functionality in the player. Most the functionality will come from the players interactions with one another. E.g. hit or miss? Number of ships left? etc.

--- BattleshipsGuestlineProject/BattleshipsEngineProj/Interfaces/IPlayer.cs
+++ BattleshipsGuestlineProject/BattleshipsEngineProj/Interfaces/IPlayer.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BattleshipsEngineProj.Interfaces
 {
     public interface IPlayer
     {
-        IList<IEntity> ActiveShips { get; set; }
+        // PROPERTY: ActiveShips to access the active ships of the player
+        IList<IEntity> ActiveShips { get; }
+        // PROPERTY: SunkShips to access the number of inactive ships
+        // IF 3 or equal to the number of the Active ships at the start of the game
+        // then the player with these ships has lost
+        int SunkShipsCount { get; }
 
-        IList<IEntity> SunkShips { get; set; }
-
+        /// <summary>
+        /// METHOD: Public method which a player will ask the other when they have fired a shot
+        /// tells them whether they have hit or missed on the location which they will pass through 
+        /// </summary>
+        /// <param name="gridPos">Grid position they are firing at</param>
+        /// <returns>Hit = true and Miss = false</returns>
+        bool HitOrMiss(int[,] gridPos);
     }
 }
---
+++ BattleshipsGuestlineProject/BattleshipsEngineProj/Player.cs
@@ -0,0 +1,48 @@
+﻿using BattleshipsEngineProj.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// AUTHOR: Harry Jones
+/// VERSION: v0.1
+/// PURPOSE: This is the player class, this will contain accessors for the ships the player
+/// has and the number of sunk ships
+/// </summary>
+namespace BattleshipsEngineProj
+{
+    public class Player : IPlayer
+    {
+        // VARIABLE a List of the Active ships which the player has which haven't been sunk
+        private IList<IEntity> _activeShips;
+        /// <summary>
+        /// PROPERTY: ActiveShips
+        /// </summary>
+        public IList<IEntity> ActiveShips { get { return _activeShips; } }
+
+        // VARIABLE to keep track of the number of ships which have been sunk
+        private int _sunkShipsCount;
+        /// <summary>
+        /// PROPERTY: SunkShipsCount
+        /// </summary>
+        public int SunkShipsCount { get { return _sunkShipsCount; }}
+
+        /// <summary>
+        /// CONSTRUCTOR for the Player class
+        /// </summary>
+        public Player() 
+        {
+            // SET the value of ships to sink to the number of active ships
+            // the player has
+        }
+
+
+        public bool HitOrMiss(int[,] gridPos) 
+        {
+            // TO CHANGE when more code is added
+            return false;
+        }
+    }
+}
