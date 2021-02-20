fa92fae131a7945b5cf889f2a1bf5abb2e87a720
Yrrah98 <harryjones101998@hotmail.com>
2021-02-20 17:21:15 +0000

    Started the foundation of building the game world.

---
+++ BattleshipsGuestlineProject/BattleshipsEngineProj/Interfaces/IGameWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsEngineProj.Interfaces
+{
+    public interface IGameWorld
+    {
+        /// <summary>
+        /// METHOD: InitialiseWorld, this method will set the world up ready
+        /// for battleships to be placed, shots to be fired and ships to be sunk. 
+        /// </summary>
+        void InitialiseWorld();
+
+        /// <summary>
+        /// METHOD: InitialiseGame, this method will set the players up ready to play when
+        /// they choose to start playing. 
+        /// </summary>
+        void InitialiseGame();
+    }
+}
