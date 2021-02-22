9c0eae97f241507fa5bdd0d049acb305fb42e7dd
Yrrah98 <harryjones101998@hotmail.com>
2021-02-22 20:28:35 +0000

    Continued work, added in the Grid class, started piecing it together. Still havent tested out any graphics, that'll be the next step. Need to hurry up more with this

---
+++ BattleshipsGuestlineProject/BattleshipsEngineProj/GameWorld/Grid/Grid.cs
@@ -0,0 +1,53 @@
+﻿using BattleshipsEngineProj.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// AUTHOR: Harry Jones
+/// DATE: 22/02/2021
+/// VERSION: v0.1
+/// PURPOSE: The grid class will be used to manage both of the players grids
+/// </summary>
+namespace BattleshipsEngineProj.GameWorld.Grid
+{
+    public class Grid
+    {
+        // VARIABLE of type ITile as a 2D array called _playerShipGrid, will be the grid to keep track of 
+        // the players own ships
+        private ITile[,] _playerShipGrid;
+        // VARIABLE of type ITile as a 2D array called _playerShotsGrid, will be the grid to keep track of
+        // the players hits and misses
+        private ITile[,] _playerShotsGrid;
+
+        /// <summary>
+        /// CONSTRUCTOR for the Grid class
+        /// </summary>
+        public Grid(ITile[,] tileArray) 
+        {
+            // SET the _playerShipGrid value to the value of Tile 2D array passed into
+            // the constructor
+            _playerShipGrid = tileArray;
+            // SET the _playerShotsGrid value to the value of Tile 2D array passed into
+            // the constructor
+            _playerShotsGrid = tileArray;
+        }
+
+        /// <summary>
+        /// METHOD: ShotfiredAt
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public bool ShotFiredAt(String position) 
+        {
+
+            // TO CHANGE - Access tile at the position passed in
+            // and call the ShootTile method on the tile
+            // RETURN the result of the check
+            return false;
+        }
+    }
+}
---
+++ BattleshipsGuestlineProject/BattleshipsEngineProj/Interfaces/IGrid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// AUTHOR: Harry Jones
+/// DATE: 22/02/2021
+/// VERSION: v0.1
+/// PURPOSE: This will act as a container for each players grids
+/// they will have a grid to place ships on and a grid to where 
+/// hits and misses will appear
+/// </summary>
+namespace BattleshipsEngineProj.Interfaces
+{
+    public interface IGrid
+    {
+        /// <summary>
+        /// METHOD: ShotFiredAt, will be a public method which the other player will call to 
+        /// take a shot at this players grid
+        /// </summary>
+        /// <param name="position">the position the other player is shooting at</param>
+        /// <returns> Returns hit or miss -> Hit = true; Miss = false </returns>
+        bool ShotFiredAt(String position);
+
+    }
+}
